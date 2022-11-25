f51c242c6c99f08090c821e65cf79f3ac7f0706b
seaflower <mikeqoo123@gmail.com>
2022-11-25 16:31:15 +0800

    請假人口

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	//"os"
@@ -15,6 +16,13 @@ const (
 	chromeDriverPath = "./chromedriver"
 	port             = 8080
 )
+
+func numberOfWeekInMonth() int {
+	now := time.Now()
+	_, w1 := time.Now().UTC().ISOWeek()
+	_, w2 := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC).UTC().ISOWeek()
+	return w1 - w2 + 1
+}
 
 func main() {
 	var concordID int
@@ -43,9 +51,19 @@ func main() {
 	}
 	defer wd.Quit()
 
-	homeURL := fmt.Sprintf("https://%d:%s@intra網址/Site2/MIS/Flow/DateTimeFlow.aspx", concordID, concordPW)
+	userURL := fmt.Sprintf("https://%d:%s@intra.concords.com.tw/Site2/main/EIP_User.aspx", concordID, concordPW)
+	wd.Get(userURL)
+	name, err := wd.FindElement(selenium.ByCSSSelector, "#Label_name") //使用者姓名
+	if err != nil {
+		panic(err)
+	}
+	nameStr, _ := name.Text()
+	nameStr = nameStr[1:10]
+	fmt.Println(nameStr)
+
+	homeURL := fmt.Sprintf("https://%d:%s@intra.concords.com.tw/Site2/MIS/Flow/DateTimeFlow.aspx", concordID, concordPW)
 	wd.Get(homeURL)
-	time.Sleep(3 * time.Second)
+	time.Sleep(1 * time.Second)
 	// page, _ := wd.PageSource()
 	// fmt.Println(page)
 	date, err := wd.FindElement(selenium.ByCSSSelector, "#DateTimeTable > tbody > tr:nth-child(1) > td.tCell_0 > span") //日期
@@ -68,14 +86,86 @@ func main() {
 	time2Str, _ := time2.Text()
 	fmt.Println(time2Str)
 
+	hrURL := fmt.Sprintf("https://%d:%s@intra.concords.com.tw/Site2/mis/hr/OverTimeListNew.aspx", concordID, concordPW)
+	wd.Get(hrURL)
+	time.Sleep(1 * time.Second)
+	css := "#Calendar1 > tbody >"
+
+	week := numberOfWeekInMonth()
+	fmt.Println("第", week, "週")
+	//看看第幾周, 去組合CSSSelector
+	/*
+		tr:nth-child(3) 第1周
+		tr:nth-child(4) 第2周
+		tr:nth-child(5) 第3周
+		tr:nth-child(6) 第4周
+		tr:nth-child(7) 第5周
+	*/
+	if week == 1 {
+		css += " tr:nth-child(3)"
+	} else if week == 2 {
+		css += " tr:nth-child(4)"
+	} else if week == 3 {
+		css += " tr:nth-child(5)"
+	} else if week == 4 {
+		css += " tr:nth-child(6)"
+	} else if week == 5 {
+		css += " tr:nth-child(7)"
+	}
+	//看看星期幾, 去組合CSSSelector
+	/*
+		td:nth-child(1) //星期天
+		td:nth-child(2) //星期一
+		td:nth-child(3)
+		td:nth-child(4)
+		td:nth-child(5)
+		td:nth-child(6)
+		td:nth-child(7) //星期六
+	*/
+	if strings.Contains(dateStr, "一") {
+		css += " td:nth-child(2)"
+	} else if strings.Contains(dateStr, "二") {
+		css += " td:nth-child(3)"
+	} else if strings.Contains(dateStr, "三") {
+		css += " td:nth-child(4)"
+	} else if strings.Contains(dateStr, "四") {
+		css += " td:nth-child(5)"
+	} else if strings.Contains(dateStr, "五") {
+		css += " td:nth-child(6)"
+	} else if strings.Contains(dateStr, "六") {
+		css += " td:nth-child(7)"
+	} else {
+		css += " td:nth-child(1)"
+	}
+
+	people, err := wd.FindElement(selenium.ByCSSSelector, css) //今天請假人數
+	if err != nil {
+		panic(err)
+	}
+	PeopleStr, _ := people.Text()
+	fmt.Println("請假名單")
+	fmt.Println("日期:", PeopleStr)
+
+	workflag := true                          //有沒有上班
+	if strings.Contains(PeopleStr, nameStr) { //有可能是請假或是代理人
+		//判斷代理人
+		b := "(" + nameStr + " 代)"
+		if strings.Contains(PeopleStr, b) {
+			workflag = true
+		} else {
+			workflag = false
+		}
+	}
+	fmt.Println()
+
 	now := time.Now()
 	hour := now.Hour()
 	minute := now.Minute()
 
 	//早上忘了打卡 幫主人打卡
-	if time1Str == " " && hour == 8 && minute <= 10 {
+	if workflag && time1Str == " " && hour == 8 && minute <= 10 {
 		fmt.Println("小精靈出現了")
-		URL := fmt.Sprintf("https://%d:%s@intra網址/site2/main/RunCard.aspx", concordID, concordPW)
+		URL := fmt.Sprintf("https://%d:%s@intra.concords.com.tw/site2/main/RunCard.aspx", concordID, concordPW)
 		wd.Get(URL)
 		btn, err := wd.FindElement(selenium.ByID, "btnSelf")
 		if err != nil {
@@ -87,9 +177,9 @@ func main() {
 	}
 
 	//下班了 幫主人先打卡
-	if hour == 17 {
+	if workflag && hour == 17 {
 		fmt.Println("小精靈出現了")
-		URL := fmt.Sprintf("https://%d:%s@intra網址/site2/main/RunCard.aspx", concordID, concordPW)
+		URL := fmt.Sprintf("https://%d:%s@intra.concords.com.tw/site2/main/RunCard.aspx", concordID, concordPW)
 		wd.Get(URL)
 		btn, err := wd.FindElement(selenium.ByID, "btnSelf")
 		if err != nil {
